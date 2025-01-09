eb29dd9e35efac871b1952a1c219ccbb73707321
Anıl DEMİR <anilaeus@gmail.com>
2025-01-09 17:07:49 +0300

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,19 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Welcome to the world of React</h1>
-    </>
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">Popular Products</section>
+      <section className="padding">Super Quality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">Special Offer</section>
+      <section className="padding bg-pale-blue">Customer Reviews</section>
+      <section className="padding sm:py-32 py-16 w-full">Subscribe</section>
+      <section className="padding bg-black padding-x padding-t pb-8">
+        Footer
+      </section>
+    </main>
   );
 }
 
